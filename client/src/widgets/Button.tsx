5a3ae0f7f99cf283a24cf780038d3022bb5bf2b2
khaalid subaan <khaalidsubaan@gmail.com>
2020-12-26 05:33:32 +0800

    widgets initial design

--- client/src/widgets/Button.tsx
+++ client/src/widgets/Button.tsx
@@ -1,11 +1,17 @@
-import PropTypes from 'prop-types'
 
-function Button() {
+
+function Button(props: ButtonProps) {
     return (
-        <div>
-
-        </div>
+        <button onClick={() => props.handler} className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
+            {props.value}
+        </button>
     )
+}
+
+interface ButtonProps {
+    handler: Function,
+    value: string,
+
 }
 
 
--- client/src/widgets/Message.tsx
+++ client/src/widgets/Message.tsx
@@ -1,13 +1,28 @@
+import React from "react"
 
 
-function Message() {
+function Message(props: MessageProp): JSX.Element {
     return (
-        <div>
-
-        </div>
+        <React.Fragment>
+            <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+                <div className="flex-shrink-0">
+                    <img className="h-12 w-12" src={`${props.image}`} alt="ChitChat Logo" />
+                </div>
+                <div>
+                    <div className="text-xl font-medium text-black">{props.user}</div>
+                    <p className="text-gray-500">{props.message}</p>
+                </div>
+            </div>
+        </React.Fragment>
     )
 }
 
+interface MessageProp {
+    message: string,
+    user: string,
+    image: string
+
+}
 
 
 export default Message

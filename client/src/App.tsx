01295a76d4c8aa7b98c462411f71e3aa2df2fef2
khaalid subaan <khaalidsubaan@gmail.com>
2020-12-28 16:52:57 +0800

    app.tsx refactored

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
 
 import { useQuery } from '@apollo/client';
-import { useState } from 'react';
-import Chat from './pages/Chat';
-import Chats from './pages/Chats';
-import Register from './pages/Register'
+import React from 'react';
+
 import { CURRENT_USER } from './utilities/schema';
 import { currentUser } from './utilities/__generated__/currentUser';
 import Sign from './pages/Sign'
+import Home from './pages/Home';
+
 
 function App() {
-
   const { data, loading, error } = useQuery<currentUser>(CURRENT_USER)
-  const [] = useState(false);
+
+
 
   const RenderSign = () => {
     return <div className='flex justify-center'>
@@ -21,28 +21,16 @@ function App() {
 
 
 
-  if (loading)
-    return <RenderSign />
-  if (error || !data)
-    return <RenderSign />
+  // if (loading)
+  //   return <RenderSign />
+  // if (error || !data)
+
+  //   return <RenderSign />
 
   return (
+    data ? <Home /> : <RenderSign />
 
-    <div className="flex justify-center">
 
-
-      <div className="md:h-2/3 md:w-4/5 h-full w-full mx-auto my-auto flex flex-row py-px border border-gray-50 bg-gray-50 rounded-3xl shadow-lg">
-        <div className="md:w-2/5 md:flex hidden">
-          <Chats />
-        </div>
-
-        <div className="w-full">
-          <Chat />
-        </div>
-
-      </div>
-
-    </div>
   );
 }
 
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import Message from '../widgets/Message'
+import Chat from './Chat'
+import Chats from './Chats'
 
 function Home() {
     return (
         <React.Fragment>
-            {/* <div className="fixed mx-auto">
-                <p>Chat</p>
-            </div> */}
+            <div className="flex justify-center">
 
-            <div className='  w-72'>
-                <Message color='green-100' message="Hello" user="John" image="https://images.unsplash.com/photo-1541250628459-d8f2f0157289?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjQzMzEwfQ&auto=format&fit=crop&w=1350&q=80" />
 
-            </div>
-            <div className='  w-72'>
-                <Message color='white' message="Hello" user="Khaalid" image="https://images.unsplash.com/photo-1541250628459-d8f2f0157289?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjQzMzEwfQ&auto=format&fit=crop&w=1350&q=80" />
+                <div className="md:h-2/3 md:w-4/5 h-full w-full mx-auto my-auto flex flex-row py-px border border-gray-50 bg-gray-50 rounded-3xl shadow-lg">
+                    <div className="md:w-2/5 md:flex hidden">
+                        <Chats />
+                    </div>
+
+                    <div className="w-full">
+                        <Chat />
+                    </div>
+
+                </div>
 
             </div>
         </React.Fragment>

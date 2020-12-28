adc035a84cafb08628ce116cb87672a10d65d135
khaalid subaan <khaalidsubaan@gmail.com>
2020-12-28 16:43:11 +0800

    render chat when logged in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,32 +7,40 @@ import Register from './pages/Register'
 import { CURRENT_USER } from './utilities/schema';
 import { currentUser } from './utilities/__generated__/currentUser';
 import Sign from './pages/Sign'
+
 function App() {
 
-  const { loading, error } = useQuery<currentUser>(CURRENT_USER)
+  const { data, loading, error } = useQuery<currentUser>(CURRENT_USER)
   const [] = useState(false);
+
+  const RenderSign = () => {
+    return <div className='flex justify-center'>
+      <Sign />
+    </div>
+  }
+
 
 
   if (loading)
-    return <Register />
-  if (error)
-    <h1>{error}</h1>
+    return <RenderSign />
+  if (error || !data)
+    return <RenderSign />
 
   return (
 
     <div className="flex justify-center">
-      <Sign />
 
-      {/* //   <div className="md:h-2/3 md:w-4/5 h-full w-full mx-auto my-auto flex flex-row py-px border border-gray-50 bg-gray-50 rounded-3xl shadow-lg">
-    //     <div className="md:w-2/5 md:flex hidden">
-    //       <Chats />
-    //     </div>
 
-    //     <div className="w-full">
-    //       <Chat />
-    //     </div>
+      <div className="md:h-2/3 md:w-4/5 h-full w-full mx-auto my-auto flex flex-row py-px border border-gray-50 bg-gray-50 rounded-3xl shadow-lg">
+        <div className="md:w-2/5 md:flex hidden">
+          <Chats />
+        </div>
 
-    //   </div> */}
+        <div className="w-full">
+          <Chat />
+        </div>
+
+      </div>
 
     </div>
   );

cfa2cfb5b0a1aa8031c1f32a7e716e5994493696
khaalid subaan <khaalidsubaan@gmail.com>
2021-01-01 21:01:13 +0800

    responsive ui implemented

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -15,6 +15,17 @@ export interface HomeProps {
 function Home(props: HomeProps) {
     const { data, loading, error } = useQuery<chats>(CHATS)
     const [currentChat, setCurrentChat] = useState('');
+    const [showChat, setshowChat] = useState(true)
+    // useEffect(() => {
+
+    //     console.log(currentChat, showChat);
+
+    // }, [currentChat])
+
+    const settingCurrentChat = (value: string) => {
+        setCurrentChat(value)
+        setshowChat(false);
+    }
 
     const { ...result } = useQuery<chatMessages, chatMessagesVariables>(CHAT_MESSAGES, { variables: { id: currentChat } })
     const subscribeToNewMessages = () =>
@@ -35,17 +46,19 @@ function Home(props: HomeProps) {
 
         })
 
+
     return (
         <React.Fragment>
             <div className="flex justify-center">
 
 
-                <div className="md:h-2/3 md:w-2/3 h-4/5 m-6  mx-auto my-auto flex flex-row py-px border border-gray-50 bg-gray-50 rounded-3xl shadow-lg">
-                    <div className="md:w-2/5 md:flex hidden">
-                        <Chats chats={data ? data.chats : []} onClick={setCurrentChat} currentUser={props.currentUser.id} />
+                <div className="md:h-2/3 md:w-2/3 w-full h-4/5 px-4  mx-auto my-auto flex flex-row py-px border border-gray-50 bg-gray-50 rounded-3xl shadow-lg">
+                    <div className={`md:w-2/5 md:flex w-full ${showChat ? '' : 'hidden'}`}>
+                        <Chats chats={data ? data.chats : []} onClick={settingCurrentChat} currentUser={props.currentUser.id} />
                     </div>
 
-                    <div className="w-full md:w-3/5 pt-3 rounded-3xl">
+                    <div className={`w-full md:w-3/5 pt-3 rounded-3xl md:block ${!showChat ? '' : 'hidden'} `}>
+                        <button className='text-customBlue pl-2 pb-2 md:hidden' onClick={() => setshowChat(true)}>Back</button>
                         {currentChat.length > 1 ? <Chat subscribeToMore={subscribeToNewMessages} chatMessages={result.data ? result.data.chatMessages : []} user={props.currentUser} currentChat={currentChat} /> : <div></div>}
                     </div>
 

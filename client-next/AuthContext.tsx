b0a3947b728e112bf9d619db312f41f1c76d6d8a
khaalid subaan <khaalidsubaan@gmail.com>
2021-01-29 22:36:26 +0800

    refactoring next localstorage

--- client-next/AuthContext.tsx
+++ client-next/AuthContext.tsx
@@ -2,9 +2,19 @@ import React from "react"
 import { useEffect } from "react"
 import { useState } from "react"
 import { AUTH_TOKEN } from "./utilities/constants"
+var isNode = typeof module !== 'undefined'
 
+const isLocalStorage = () => {
+    if (isNode) {
+        if (typeof (localStorage) !== undefined)
+            return localStorage.getItem(AUTH_TOKEN)
+        return ''
+    }
+    return ''
+
+}
 const defaultContext = {
-    auth: '',
+    auth: isLocalStorage(),
     setAuth: (auth: any, getCurrentUser: any) => { },
 
 }
--- client-next/pages/_app.tsx
+++ client-next/pages/_app.tsx
@@ -1,50 +1,23 @@
 
 import "tailwindcss/tailwind.css";
 import '../styles/globals.css'
+import { useContext, useEffect } from 'react';
 import { ApolloClient, HttpLink, ApolloProvider, InMemoryCache, split, NormalizedCacheObject } from "@apollo/client";
 import { WebSocketLink } from "@apollo/client/link/ws";
 import { getMainDefinition } from "@apollo/client/utilities";
 import { SubscriptionClient } from "subscriptions-transport-ws";
-// import { authHttpLink, AUTH_TOKEN } from "./utilities/constants";
-let cache = new InMemoryCache();
-
-const api = process.env.NODE_ENV === 'development' ? 'localhost/chat/graphql' : 'localhost/chat/graphql';
-const ws = process.env.NODE_ENV === 'development' ? 'localhost/chat/graphql' : 'localhost/chat/graphql';
-
-
-
-// const wsLink =
-//   new WebSocketLink({
-
-//     uri: `ws://${ws}`,
-//     options: {
-//       reconnect: true,
-//       connectionParams: () => ({
-//         authorization: `Bearer `,
-//       }),
-//     },
-//   })
-
-const httpLink = new HttpLink({
-  uri: `http://${api}`,
-
-})
-
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
-  },
-  // wsLink,
-  httpLink
-);
-const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({ cache: cache, link: splitLink })
+import { useClient } from '../ApolloClient';
+import { AuthContext, AuthContextProvider } from '../AuthContext';
+import { authHttpLink, AUTH_TOKEN } from "../utilities/constants";
 function MyApp({ Component, pageProps }) {
+  const { auth } = useContext(AuthContext)
+  const authClient = useClient();
+  useEffect(() => {
+    console.log('in renderApp :', auth);
+  }, [auth])
+
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={authClient}>
       <div className='flex justify-center'>
         <Component {...pageProps} />
       </div>
